7d12b39c8e18c5d6697784e6f77b39fc1d1407eb
Hasnain Merchant <hasnainmerchant0@gmail.com>
2024-10-26 20:38:10 +0530

    Some code modifications to improve functionality

--- palm-client/src/App.jsx
+++ palm-client/src/App.jsx
@@ -1,27 +1,47 @@
-import { useState, useEffect } from "react"
+import { useState, useRef } from "react"
+import DOMPurify  from "dompurify";
+import { marked } from "marked";
+
+marked.use({
+  gfm: true,
+});
 
 function App() {
 
-  const [serverData, setServerData] = useState('');
+  const [serverData, setServerData] = useState([{}]);
   const [userPrompt, setUserPrompt] = useState('');
+  const inputRef = useRef(null);
 
   function handleSubmit() {
-    fetch('/api', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({'prompt': userPrompt})
-    })
+    setServerData('');
+    if (userPrompt !== '') {
+      fetch('/api', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({'prompt': userPrompt})
+      })
       .then((res) => res.json())
       .then((data) => {
-        setServerData(data)
-        console.log(data);
+        const html = DOMPurify.sanitize(marked(data));
+        setServerData({...data, html});
+        inputRef.current.focus();
+        setUserPrompt('');
       });
+    }
   }
 
   function handleInput(event) {
     setUserPrompt(event.target.value);
+  }
+
+  function handleKeyDown(event) {
+    if (event.key === 'Enter' && !event.shiftKey) {
+      event.preventDefault();
+      inputRef.current.blur();
+      handleSubmit();
+    }
   }
 
   return (
@@ -29,13 +49,13 @@ function App() {
       <h1 style={{padding: '10px', marginBottom: '0'}}>MyPrompter</h1>
       <div style={{margin: '0', flexGrow: '1', overflowY: 'scroll'}}>
         <div style={{width: '100%', height: '100%'}}>
-          <article style={{margin: '0'}}>
-            {serverData}
-          </article>
+          {
+            (serverData === '') ? ('Loading...'): <article style={{margin: '0'}} dangerouslySetInnerHTML={{__html: serverData.html}} />
+          }
         </div>
       </div>
       <div style={{display: 'flex', alignItems: 'end', backgroundColor: '#222', padding: '10px'}}>
-        <textarea onChange={handleInput} style={{margin: '0', flexGrow: '1', overflowY: 'hidden'}} placeholder="Type In Your Prompt"/>
+        <textarea onKeyDown={handleKeyDown} ref={inputRef} onChange={handleInput} value={userPrompt} style={{margin: '0', flexGrow: '1', overflowY: 'hidden'}} placeholder="Type In Your Prompt"/>
         <button style={{margin: '0', flex: '1', marginLeft: '10px'}} onClick={handleSubmit}>Go</button>
       </div>
     </main>
