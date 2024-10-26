7014831c5517486f53bf38f461bc8813d1974d47
Hasnain Merchant <hasnainmerchant0@gmail.com>
2024-10-26 18:58:57 +0530

    Submit prompt to your API

--- palm-client/src/App.jsx
+++ palm-client/src/App.jsx
@@ -3,20 +3,31 @@ import { useState, useEffect } from "react"
 function App() {
 
   const [serverData, setServerData] = useState('');
+  const [userPrompt, setUserPrompt] = useState('');
 
-  useEffect(() => {
-    fetch("/api")
+  function handleSubmit() {
+    fetch('/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({'prompt': userPrompt})
+    })
       .then((res) => res.json())
       .then((data) => {
         setServerData(data)
         console.log(data);
-      })
-  }, []);
+      });
+  }
+
+  function handleInput(event) {
+    setUserPrompt(event.target.value);
+  }
 
   return (
     <main style={{height: '100vh', display: 'flex', flexDirection: 'column'}}>
       <h1 style={{padding: '10px', marginBottom: '0'}}>MyPrompter</h1>
-      <div style={{margin: '0', flexGrow: '1'}}>
+      <div style={{margin: '0', flexGrow: '1', overflowY: 'scroll'}}>
         <div style={{width: '100%', height: '100%'}}>
           <article style={{margin: '0'}}>
             {serverData}
@@ -24,8 +35,8 @@ function App() {
         </div>
       </div>
       <div style={{display: 'flex', alignItems: 'end', backgroundColor: '#222', padding: '10px'}}>
-        <textarea style={{margin: '0', flexGrow: '1', overflowY: 'hidden'}} placeholder="Type In Your Prompt"/>
-        <button style={{margin: '0', flex: '1', marginLeft: '10px'}}>Go</button>
+        <textarea onChange={handleInput} style={{margin: '0', flexGrow: '1', overflowY: 'hidden'}} placeholder="Type In Your Prompt"/>
+        <button style={{margin: '0', flex: '1', marginLeft: '10px'}} onClick={handleSubmit}>Go</button>
       </div>
     </main>
   )
--- palm-server/index.js
+++ palm-server/index.js
@@ -3,18 +3,18 @@ require('dotenv').config();
 //Create an express server
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
 app.listen(3333, () => console.log('Server Running On Port: 3333'));
-
-
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-var answer = null;
-const prompt = "With additional words of your own Repeat after me: one, two,";
+let answer = null;
 
-
-app.get('/api', (req, res) => {
+app.post('/api', (req, res) => {
+    let prompt = req.body.prompt;
     async function generateStory() {
         const result = await model.generateContent(prompt);
         answer = result.response.text();
