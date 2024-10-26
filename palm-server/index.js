2e88bb66b7c66aeb258f2326cd3d6929cb581eb8
Hasnain Merchant <hasnainmerchant0@gmail.com>
2024-10-26 18:13:51 +0530

    Installing React and using Vite framework

--- palm-server/index.js
+++ palm-server/index.js
@@ -1,13 +1,24 @@
 require('dotenv').config();
+
+//Create an express server
+const express = require('express');
+const app = express();
+app.listen(3333, () => console.log('Server Running On Port: 3333'));
+
+
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.API_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
+var answer = null;
 const prompt = "With additional words of your own Repeat after me: one, two,";
 
-async function generateStory() {
-    const result = await model.generateContent(prompt);
-    console.log(result.response.text());   
-}
 
-generateStory();
+app.get('/api', (req, res) => {
+    async function generateStory() {
+        const result = await model.generateContent(prompt);
+        answer = result.response.text();
+        res.json(answer);
+    }
+    generateStory();
+});
